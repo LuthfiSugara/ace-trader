3cda07e95f21a58550e08f78918fef28ff8877aa
luthfi sugara <sugaraluthfi@gmail.com>
2025-06-04 11:55:10 +0700

    create hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
+import { Hero, Image } from "@/components";
 
 
 export default function Home() {
+
+  
   return (
-    <div className="h-[1000px]">
-      <div className="pt-[100px] pb-[200px]" style={{background: 'linear-gradient(180deg, #118C8A 0%, #072B33 80.29%)'}}>
-        <div className="p-[60px]">
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <div className="h-[2000px]">
+      <Hero />
+      {/* <div className="pt-[30px] pb-[200px]" style={{background: 'linear-gradient(180deg, #118C8A 0%, #072B33 80.29%)'}}>
+        <div className="p-[60px] grid grid-cols-1 md:grid-cols-5 gap-8">
+          <div className="col-span-3 space-y-[32px] my-auto">
+            <h2 className="text-[56px] leading-[72px] font-bold text-white">Trade our capital. Keep up to 90% profits</h2>
+            <p className="text-[#BDF6FF]">Join Ace Trade and get instant access to funding with zero personal risk. Scale your trading, maximize your profits, and grow without limits.</p>
+            <div className="flex gap-4">
+              <a href="" className="bg-[#05CBE9] text-[#072B33] py-2 px-4 rounded-full">Start Challenge</a>
+              <a href="" className="border-[2px] border-white rounded-full py-2 px-4 text-white">Learn More</a>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <Image src='/images/Banner.png' alt="hero" width={800} height={800} className="w-full" />
+          </div>
 
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- components/index.tsx
+++ components/index.tsx
@@ -1,9 +1,11 @@
 import Image from "@/components/Image";
 import Header from "@/components/Header";
 import ImageExternal from "@/components/ImageExternal";
+import Hero from "@/components/Hero";
 
 export {
     Image,
     Header,
     ImageExternal,
+    Hero,
 }
