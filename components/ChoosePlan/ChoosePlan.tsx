f3b5d9b0e3f706929f94ca30138854b72118ddc4
luthfi sugara <sugaraluthfi@gmail.com>
2025-06-07 22:01:04 +0700

    fix unused api

--- components/ChoosePlan/ChoosePlan.tsx
+++ components/ChoosePlan/ChoosePlan.tsx
@@ -29,32 +29,6 @@ const ChoosePlan = () => {
     const {isOpen, onClose, onOpen} = useDisclosure(),
     [planInformation, setPlanInformation] = useState<PlansProps>(),
     [information, setInformation] = useState<string[]>([""]);
-    
-    // const infoPlan = {
-    //     "profit_target": [
-    //         "To prove your trading skills, you have to reach the profit target after closing all your trades without violating any trading rules within the given trading period."
-    //     ],
-    //     "maximum_loss": [
-    //         "The Maximum Loss Limit is the amount your equity or balance can't go below. This rule is set to 10% of the initial account size. For example, if you have a $100.000 account and the Maximum Loss Limit is 10%, your equity or balance can't go below $90.000 at any moment."
-    //     ],
-    //     "maximum_daily_loss": [
-    //         "The Maximum Daily Loss is the amount you are allowed to lose every day.",
-    //         "<br />",
-    //         "For the purpose of this rule, the higher value between equity and balance will be used. This rule is set as a % of the starting equity or balance of every day. The rule states that the equity of the day, which is the result of the currently floating PnL (Profit and Loss) in sum with all closed positions of that day must not hit the Maximum Daily Loss Limit.",
-    //         "<br />",
-    //         "Example:",
-    //         "You have a master account. At the start of day 5, your account balance is $105.000 and your equity is $107.000. The Daily Loss Limit is 5% from the starting equity for this example, this means that your equity on day 5 can't go lower than: Maximum Daily loss = $107.000 starting equity balance * 5% Daily Loss Limit = $5.350 Daily Loss Limit. $107.000 starting equity balance on day 5 - $5.350 Daily Loss Limit= $101.650 If your equity goes below $101.650 at any certain moment of time on day 5 your account will be closed."
-    //     ],
-    //     "minimum_trading_days": [
-    //         "During each stage of our evaluation, there are a minimum of 3 trading days. A day is counted as a trading day if at least one trade is executed. Holding a trade over multiple days will not count as multiple trading days.",
-    //         "<br />",
-    //         "Note:",
-    //         "If you reach the profit target before completing the 3 minimum trading days, it is allowed to execute 0.01 lot trades on the remaining days."
-    //     ],
-    //     "leverage": [
-    //         "Trade with up to 1:100 leverage with RAW Spreads. There is a different leverage applied for different types of financial instruments.FX 1:100, Indices 1:20, Metals 1:30, Energies 1:10 & Crypto 1:2."
-    //     ]
-    // }
 
     useEffect(() => {
         fetch('/data/info-plan.json')
@@ -74,45 +48,44 @@ const ChoosePlan = () => {
         <div className='target-element mt-[100px] space-y-8' id='plans'>
             <h2 className='text-[30px] lg:text-[40px] text-white font-bold text-center'>Choose the plan that fits you!</h2>
 
-            <div className='flex flex-wrap gap-[32px] bg-[#06333D] rounded-xl p-[32px]'>
+            <div className='flex flex-wrap gap-[32px] bg-[#06333D] rounded-xl p-[16px] md:p-[32px]'>
                 <div className='space-y-3'>
                     <p className='text-white font-semibold'>Plan</p>
                     <div className='flex flex-wrap gap-4'>
-                        {}
-                        <Button className='bg-[#05CBE966] text-white border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>Standard</Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>Crypto</Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>Instant</Button>
+                        <Button className='bg-[#05CBE966] text-white border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-xl hover:bg-[#05CBE966] hover:text-white'>Standard</Button>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-xl hover:bg-[#05CBE966] hover:text-white'>Crypto</Button>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-xl hover:bg-[#05CBE966] hover:text-white'>Instant</Button>
                     </div>
                 </div>
                 <div className='space-y-3'>
                     <p className='text-white font-semibold'>Account Balance</p>
                     <div className='flex flex-wrap gap-4'>
-                        <Button className='bg-[#05CBE966] text-white border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>5K</Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>10K</Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>25K</Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>50K</Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>100K</Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>250K</Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>450K</Button>
+                        <Button className='bg-[#05CBE966] text-white border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>5K</Button>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>10K</Button>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>25K</Button>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>50K</Button>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>100K</Button>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>250K</Button>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>450K</Button>
                     </div>
                 </div>
                 <div className='space-y-3'>
                     <p className='text-white font-semibold'>Product Type</p>
                     <div className='flex flex-wrap gap-4'>
-                        <Button className='bg-[#05CBE966] text-white border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>1-Step</Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>2-Step</Button>
+                        <Button className='bg-[#05CBE966] text-white border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>1-Step</Button>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>2-Step</Button>
                     </div>
                 </div>
                 <div className='space-y-3'>
                     <p className='text-white font-semibold'>Platform</p>
                     <div className='flex flex-wrap gap-4'>
-                        <Button className='bg-[#05CBE966] text-white border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>
+                        <Button className='bg-[#05CBE966] text-white border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>
                             <Image src='/icons/Ctrader.png' alt='ctrader' width={100} height={100} className='w-[100px]' />
                         </Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>
                             <Image src='/icons/dxtrade.png' alt='dxtrade' width={100} height={100} className='w-[100px]' />
                         </Button>
-                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[16px] rounded-[16px] hover:bg-[#05CBE966] hover:text-white'>
+                        <Button className='text-[#BDF6FF] border border-[#3AA7B8] p-[8px] md:p-[12px] rounded-[12px] hover:bg-[#05CBE966] hover:text-white'>
                             <Image src='/icons/match-trader.png' alt='match trader' width={100} height={100} className='w-[100px]' />
                         </Button>
                     </div>
